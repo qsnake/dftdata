340d8250535c9567927023b7bea3d26827b1052f
Ondrej Certik <ondrej@certik.cz>
2011-02-23 15:26:02 -0800

    Parse the orig.html correctly

--- ion_energy/parse.py
+++ ion_energy/parse.py
@@ -1,2 +1,24 @@
-from xml.etree import ElementTree as ET
-ET.parse("orig.html")
+from json import dump
+
+def trim(s, v):
+    i = s.find(v)
+    s = s[i+len(v):]
+    return s
+
+s = open("orig.html").read()
+s = s.replace("&nbsp;", "")
+energies = {}
+for Z in range(1, 105):
+    s = trim(s, '<TR VALIGN="baseline">')
+    s = trim(s, '<TD ALIGN="RIGHT">')
+    _Z = int(s[:s.find("<")])
+    assert _Z == Z
+    if Z == 85:
+        continue
+    s = trim(s, '<TD ALIGN="right">')
+    E = (s[:s.find("<")]).strip()
+    E = E.replace("?", "")
+    energies[Z] = E
+
+f = open("ion_energies.json", "w")
+dump(energies, f)
